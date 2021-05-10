49cca3e5dc4b3c359898182aaa3fc370e5af1ea8
shiwam <shiwam@cse.iitk.ac.in>
2021-05-10 10:55:59 +0530

    Config file

---
+++ config.py
@@ -0,0 +1,32 @@
+# d4j_repo='Math'
+# d4j_repo_lower_case='math'
+# d4j_repo_bugs=106
+# num_coverage=5
+
+THRESHOLD = 0.2
+MAX_REFINEMENTS = 10
+
+# d4j_repo='Chart'
+# d4j_repo_lower_case='chart'
+# d4j_repo_bugs=26
+# num_coverage=5
+
+# d4j_repo='Closure'
+# d4j_repo_lower_case='closure'
+# d4j_repo_bugs=133
+# num_coverage=5
+
+# d4j_repo='Lang'
+# d4j_repo_lower_case='lang'
+# d4j_repo_bugs=65
+# num_coverage=5
+
+# d4j_repo='Mockito'
+# d4j_repo_lower_case='mockito'
+# d4j_repo_bugs=38
+# num_coverage=5
+
+d4j_repo='Time'
+d4j_repo_lower_case='time'
+d4j_repo_bugs=27
+num_coverage=5
